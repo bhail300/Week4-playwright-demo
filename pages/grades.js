7ebe1099960004ea4ec996c56c5d714fc2e35685
bhail300 <bhaile300@gmail.com>
2023-01-24 11:11:51 -0800

    new files and folders

---
+++ pages/grades.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Grades(){
+    return (
+        <>
+        <p>Grades Page</p>
+        <Link href="/"> Go Back </Link>
+        </>
+    )
+}
